c9619578dc27fcbe23e8001a044db4f22abe9c7c
pyrce <fetheve2@gmail.com>
2020-03-01 18:15:58 +0400

    correction bug page

--- route/router.js
+++ route/router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express();
 const operationController = require("../controller/UrlController.js");
 
+router.get("/ajout", operationController.ajout);
 router.get("/", operationController.list);
 router.get("/:page", operationController.list);
-router.get("/ajout", operationController.ajout);
 router.get("/encode/:item", operationController.encode);
 router.post("/add", operationController.add);
 router.get("/voir/:id", operationController.voir);
